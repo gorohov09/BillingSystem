7ab4ea7166089b1496025666dd738a79d463bb44
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-04 23:09:15 +0500

    Добавление тестовых данных + интерфейс инициализации БД

---
+++ Services/BillingSystem.Interfaces/IDbInitializer.cs
@@ -0,0 +1,9 @@
+﻿namespace BillingSystem.Interfaces
+{
+    public interface IDbInitializer
+    {
+        Task<bool> RemoveAsync(CancellationToken cancel = default);
+
+        Task InitializeAsync(bool RemoveBefore = false, CancellationToken cancel = default);
+    }
+}
---
+++ Services/BillingSystem.Services/Data/TestData.cs
@@ -0,0 +1,14 @@
+﻿using BillingSystem.Domain.Entities;
+
+namespace BillingSystem.Services.Data
+{
+    public static class TestData
+    {
+        public static IEnumerable<UserProfileDomain> UserProfiles { get; } = new List<UserProfileDomain>()
+        {
+            new UserProfileDomain() {Name = "boris", Rating = 5000},
+            new UserProfileDomain() {Name = "maria", Rating = 1000},
+            new UserProfileDomain() {Name = "oleg", Rating = 800},
+        };
+    }
+}
