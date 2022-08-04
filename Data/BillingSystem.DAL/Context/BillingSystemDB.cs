d1eb0f3961dc9f6a1760f88a3162094dfe3886a7
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-04 22:46:26 +0500

    Создание контекста БД

---
+++ Data/BillingSystem.DAL/Context/BillingSystemDB.cs
@@ -0,0 +1,17 @@
+﻿using BillingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillingSystem.DAL.Context
+{
+    public class BillingSystemDB : DbContext
+    {
+        public DbSet<CoinDomain> Coins { get; set; }
+
+        public DbSet<UserProfileDomain> UserProfiles { get; set; }
+
+        public BillingSystemDB(DbContextOptions<BillingSystemDB> options)
+            : base(options)
+        {
+        }
+    }
+}
