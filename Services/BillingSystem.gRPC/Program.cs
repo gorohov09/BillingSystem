43a86f12101b2f4b978f63a1eb1fa79567d3b26b
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-04 23:57:23 +0500

    Регистрация сервиса

--- Services/BillingSystem.gRPC/Program.cs
+++ Services/BillingSystem.gRPC/Program.cs
@@ -23,6 +23,7 @@ await using (var scope = app.Services.CreateAsyncScope())
 }
 
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<BillingService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
