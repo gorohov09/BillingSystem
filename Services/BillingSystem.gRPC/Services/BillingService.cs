3d54e583a2b5fcbfe11705190491cf32d8715c0f
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-04 23:57:13 +0500

    Реализация в сервисе метода получения профилей пользователей

--- Services/BillingSystem.gRPC/Services/BillingService.cs
+++ Services/BillingSystem.gRPC/Services/BillingService.cs
@@ -1,20 +1,32 @@
 ﻿using Billing;
+using BillingSystem.DAL.Context;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace BillingSystem.gRPC.Services
 {
     public class BillingService : Billing.Billing.BillingBase
     {
         private readonly ILogger<BillingService> _logger;
+        private readonly BillingSystemDB _db;
 
-        public BillingService(Logger<BillingService> logger)
+        public BillingService(ILogger<BillingService> logger, BillingSystemDB db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public override Task ListUsers(None request, IServerStreamWriter<UserProfile> responseStream, ServerCallContext context)
+        public override async Task ListUsers(None request, IServerStreamWriter<UserProfile> responseStream, ServerCallContext context)
         {
-            return base.ListUsers(request, responseStream, context);
+            var userProfilies = await _db.UserProfiles.ToListAsync();
+            foreach (var profile in userProfilies)
+            {
+                await responseStream.WriteAsync(new UserProfile
+                {
+                    Name = profile.Name,
+                    Amount = profile.Amount ?? 0,
+                });
+            }
         }
 
         public override Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
