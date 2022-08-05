5627d2916f2b17c5c2582fff2b94282cbeeed18a
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-05 22:48:20 +0500

    Добавление класса, отвечающего за статус операции

---
+++ Common/BillingSystem.Domain/ViewModels/ResponseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Domain.ViewModels
+{
+    public class ResponseViewModel
+    {
+        public enum Status
+        {
+            STATUS_UNSPECIFIED,
+            STATUS_OK,
+            STATUS_FAILED
+        }
+
+        public Status StatusOperation { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
--- Services/BillingSystem.Interfaces/IBillingRepository.cs
+++ Services/BillingSystem.Interfaces/IBillingRepository.cs
@@ -1,14 +1,11 @@
 ﻿using BillingSystem.Domain.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BillingSystem.Interfaces
 {
     public interface IBillingRepository
     {
         Task<IEnumerable<UserProfileViewModel>> GetUserProfiliesVm();
+
+        Task
     }
 }
