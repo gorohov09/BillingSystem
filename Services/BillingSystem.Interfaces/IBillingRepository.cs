b9dc870e58f34a2f4b214e80c2d402aedb4e8f57
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-08-05 22:19:45 +0500

    Создание интерфейса для паттерна - репозиторий

---
+++ Services/BillingSystem.Interfaces/IBillingRepository.cs
@@ -0,0 +1,14 @@
+﻿using BillingSystem.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Interfaces
+{
+    public interface IBillingRepository
+    {
+        Task<UserProfileViewModel> GetUserProfilies();
+    }
+}
